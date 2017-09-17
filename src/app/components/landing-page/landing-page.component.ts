2bb9cfd9b81cf50138b225bca35f00e04ecc0abf
Maggie McCain <maggie.mccain13@gmail.com>
2017-09-17 16:13:03 -0400

    refactored navigation and auth0 updates

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-landing-page',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth: AuthService) { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  isAuthenticated(): boolean {
+	return this.auth.isAuthenticated()
   }
-
 }
